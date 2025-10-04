e62f76f72a2eab219f49a515e7469e4574fe14fb
erickromao <erickromao50@gmail.com>
2025-10-04 13:00:33 -0300

    feat: Adicionando as telas de aulas e calendario (vazias)

---
+++ frontend/lib/app/features/community/screens/aulas_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AulasTab extends StatelessWidget {
+  const AulasTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Aulas', style: TextStyle(color: Colors.white, fontSize: 24)),
+    );
+  }
+}
---
+++ frontend/lib/app/features/community/screens/calendario_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CalendarioTab extends StatelessWidget {
+  const CalendarioTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Calendário', style: TextStyle(color: Colors.white, fontSize: 24)),
+    );
+  }
+}
