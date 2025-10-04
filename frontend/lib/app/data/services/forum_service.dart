650c413c656194f70332bed93200d208d42326bf
erickromao <erickromao50@gmail.com>
2025-10-04 13:34:46 -0300

    feat: Consumindo endpoint da api para criacao de post

--- frontend/lib/app/data/services/forum_service.dart
+++ frontend/lib/app/data/services/forum_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:frontend/app/data/models/post.dart';
+import 'package:frontend/app/data/models/post_create.dart';
 
 class ForumService {
   final String _baseUrl = 'http://192.168.1.7:5000/api/v1'; // Adjust if your API is hosted elsewhere
@@ -14,6 +15,20 @@ class ForumService {
       return posts;
     } else {
       throw Exception('Failed to load posts');
+    }
+  }
+
+  Future<Post> createPost(PostCreate post) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/forum/posts'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(post.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return Post.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create post');
     }
   }
 }
