6e766fea60e6f28560ef387302c681fddcc5804a
erickromao <erickromao50@gmail.com>
2025-10-04 12:27:01 -0300

    feat: Adicionando posts para forum

--- frontend/lib/app/data/services/forum_service.dart
+++ frontend/lib/app/data/services/forum_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:frontend/app/data/models/post.dart';
 
 class ForumService {
-  final String _baseUrl = 'http://192.168.1.7:5000'; // Adjust if your API is hosted elsewhere
+  final String _baseUrl = 'http://192.168.1.7:5000/api/v1'; // Adjust if your API is hosted elsewhere
 
   Future<List<Post>> getPosts() async {
     final response = await http.get(Uri.parse('$_baseUrl/forum/posts'));
--- frontend/lib/app/data/services/user_service.dart
+++ frontend/lib/app/data/services/user_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:frontend/app/data/models/user.dart';
 
 class UserService {
-  final String _baseUrl = 'http://localhost:5000'; // Adjust if your API is hosted elsewhere
+  final String _baseUrl = 'http://192.168.1.7:5000/api/v1'; // Adjust if your API is hosted elsewhere
 
   Future<User> getUser(int userId) async {
     final response = await http.get(Uri.parse('$_baseUrl/users/$userId'));
